03a656f5f5f72acf3c3eef6c8232afe1dd089466
melindapp <136713006+melindapp@users.noreply.github.com>
2023-06-16 20:03:52 +0700

    Create Latihan1.py
    Latihan 1 Algoritma Pemrograman

---
+++ Latihan1.py
@@ -0,0 +1,23 @@
+nim = input ("masukan nim anda") 
+
+nama = input ("Masukan nama anda")
+
+kelas = input ("Masukan kelas anda")
+
+usia = input ("Masukan usia anda")
+
+print ("================================") 
+
+print ("Indentitas mahasiswa")
+
+print ("================================") 
+
+print ("NIM Mahasiswa :",nim)
+
+print ("Nama lengkap  :",nama)
+
+print ("kelas saat ini:",kelas)
+
+print ("usia anda     :",usia)
+
+print("=================================")
